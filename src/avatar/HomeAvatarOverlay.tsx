7389e2e175a21886e28c71b8f4ba587787446b39
Ján Švehla <jan.svehla@pm.me>
2026-04-21 22:46:29 +0200

    fix: minimize mobile avatar overlay

--- src/avatar/HomeAvatarOverlay.tsx
+++ src/avatar/HomeAvatarOverlay.tsx
@@ -4,7 +4,7 @@ export function HomeAvatarOverlay() {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none fixed bottom-0 right-0 z-10 h-[24svh] min-h-[140px] w-[min(24vw,220px)] min-w-[120px] translate-x-[26%] translate-y-[10%] sm:h-[30svh] sm:min-h-[180px] sm:w-[min(22vw,280px)] sm:min-w-[150px] sm:translate-x-[20%] sm:translate-y-[8%] lg:h-[38svh] lg:min-h-[260px] lg:w-[min(20vw,360px)] lg:min-w-[180px] lg:translate-x-[16%] lg:translate-y-[6%]"
+      className="pointer-events-none fixed bottom-0 right-0 z-10 h-[18svh] min-h-[90px] w-[min(18vw,160px)] min-w-[76px] translate-x-[45%] translate-y-[20%] sm:h-[24svh] sm:min-h-[130px] sm:w-[min(18vw,220px)] sm:min-w-[110px] sm:translate-x-[38%] sm:translate-y-[16%] lg:h-[34svh] lg:min-h-[220px] lg:w-[min(18vw,320px)] lg:min-w-[150px] lg:translate-x-[24%] lg:translate-y-[10%]"
     >
       <AvatarPresenter className="h-full w-full" />
     </div>
