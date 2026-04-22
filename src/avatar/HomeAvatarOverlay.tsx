773c2e9f725b37fb6762742b74eecad9df683649
Ján Švehla <jan.svehla@pm.me>
2026-04-22 22:26:19 +0200

    fix: reduce phone avatar overlay footprint

--- src/avatar/HomeAvatarOverlay.tsx
+++ src/avatar/HomeAvatarOverlay.tsx
@@ -4,7 +4,7 @@ export function HomeAvatarOverlay() {
   return (
     <div
       aria-hidden="true"
-      className="pointer-events-none fixed bottom-0 right-0 z-10 h-[18svh] min-h-[90px] w-[min(18vw,160px)] min-w-[76px] translate-x-[45%] translate-y-[20%] sm:h-[24svh] sm:min-h-[130px] sm:w-[min(18vw,220px)] sm:min-w-[110px] sm:translate-x-[38%] sm:translate-y-[16%] lg:h-[34svh] lg:min-h-[220px] lg:w-[min(18vw,320px)] lg:min-w-[150px] lg:translate-x-[24%] lg:translate-y-[10%]"
+      className="pointer-events-none fixed bottom-0 right-0 z-10 h-[14svh] min-h-[60px] w-[min(14vw,120px)] min-w-[48px] translate-x-[62%] translate-y-[32%] sm:h-[24svh] sm:min-h-[130px] sm:w-[min(18vw,220px)] sm:min-w-[110px] sm:translate-x-[38%] sm:translate-y-[16%] lg:h-[34svh] lg:min-h-[220px] lg:w-[min(18vw,320px)] lg:min-w-[150px] lg:translate-x-[24%] lg:translate-y-[10%]"
     >
       <AvatarPresenter className="h-full w-full" />
     </div>
